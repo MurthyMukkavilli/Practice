38547960a46e77cadcd6e47475ad73c7b7bad4e4
MurthyMukkavilli <43471200+MurthyMukkavilli@users.noreply.github.com>
2018-10-04 17:46:07 +0530

    Update ActivityAssign.java

--- ActivityAssign.java
+++ ActivityAssign.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class ActivityAssign {
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
+		
        Scanner sc=new Scanner(System.in);
        System.out.println("Enter the array size");
        int n=sc.nextInt();
