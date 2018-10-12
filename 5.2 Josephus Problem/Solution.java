cf5a4ccbb6a5785732c676cfdc4d235dead462a4
MurthyMukkavilli <43471200+MurthyMukkavilli@users.noreply.github.com>
2018-10-12 12:03:04 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

---
+++ 5.2 Josephus Problem/Solution.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+
+class Node1
+{
+	Object data;
+	Node1 next;
+	Node1(int data)
+	{
+		this.data=data;
+		next=null;
+	}
+}
+class Queue1
+{
+	Node1 head;
+	Node1 tail;
+	boolean isempty()
+	{
+		return head==null;
+	}
+	void Queue(int data)
+	{
+		Node1 n=new Node1(data);
+		if(isempty())
+		{
+			head=tail=n;
+			
+		}
+		else
+		{
+			tail.next=n;
+			tail=tail.next;
+		}
+				
+	}
+	int Dequeue()
+	{
+		Node1 temp=head;
+		
+		if(isempty())
+		{
+			System.out.println("No elements");
+		}
+		else
+		{
+		head=temp.next;
+		}
+		return (int)temp.data;
+	}
+	
+}
+public class Solution{
+
+	 public static void main(String[] args) {
+		 Scanner sc=new Scanner(System.in); 
+		 System.out.println("Enter K");
+		 int k=sc.nextInt();
+		 System.out.println("m and n");
+		 while(sc.hasNext())
+		 {
+	        int m = sc.nextInt();
+	        int n= sc.nextInt();
+
+	        // initialize the queue
+	        Queue1 l =  new Queue1(); 
+	        for (int i = 0; i < m; i++)
+	        {
+	            l.Queue(i);
+	        }
+
+	      while (!l.isempty()) 
+	      {
+		for (int i = 0; i < m-1; i++)
+	        {
+	            for (int j = 0; j < n-1; j++) 
+	            {
+	                l.Queue(l.Dequeue());
+	            }
+	           System.out.print(l.Dequeue()+" ");
+	        } 
+			l.Queue(l.Dequeue());
+	       System.out.print(l.Dequeue());
+	        System.out.println();
+	        k++;
+		 }
+	    }
+
+}
+}
